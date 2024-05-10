b53dbfc00a2b18fbd905359ca3cfa5fb82c562ac
prunuspadus <annaminaeva2004@gmail.com>
2024-05-10 21:55:11 +0300

    Поправила сообщения для вывода пользователю

--- client.py
+++ client.py
@@ -6,15 +6,15 @@ PORT = 8888
 async def tcp_echo_client(message):
     reader, writer = await asyncio.open_connection(HOST, PORT)
 
-    message = input("Введите сообщение для отправки серверу: ")
     print(f'Отправка данных серверу: {message}')
     writer.write(message.encode())
 
     data = await reader.read(100)
-    print(f'Received: {data.decode()}')
+    print(f'Получено: {data.decode()}')
 
-    print('Closing the connection')
+    print('Остновка соединения')
     writer.close()
     await writer.wait_closed()
 
-asyncio.run(tcp_echo_client('Hello, World!'))
+message = input("Введите сообщение для отправки серверу: ")
+asyncio.run(tcp_echo_client(message))
